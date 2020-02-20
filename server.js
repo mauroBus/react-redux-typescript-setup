b35f837a01a992cc2cb1e92fbb53e30cc4466056
Mauro Buselli <maurobuselli@gmail.com>
2020-02-20 03:08:58 -0300

    Static Node server to serve the built files

---
+++ server.js
@@ -0,0 +1,17 @@
+var static = require('node-static')
+
+var port = process.env.PORT || 3000
+var staticServer = new static.Server('./dist')
+
+require('http')
+  .createServer(function(request, response) {
+    request
+      .addListener('end', function() {
+        staticServer.serve(request, response)
+      })
+      .resume()
+  })
+  .listen(port, () => {
+    console.log('Success!  👍')
+    console.log(`Server running at http://localhost:${port}`)
+  })
